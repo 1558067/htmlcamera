3ddb9b47a71c4fda5d84518dedc7be774e6bff0a
1558067 <97866782+1558067@users.noreply.github.com>
2023-06-15 21:52:40 +0900

    bug fix on ios

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
+var landscapeInput = document.getElementById("landscape");
+var personInput = document.getElementById("person");
+var foodInput = document.getElementById("food");
+var animationInput = document.getElementById("animation");
+
+landscapeInput.addEventListener("change", function () {
+  showThumbnail(landscapeInput, "thumbnail-landscape");
+});
+
+personInput.addEventListener("change", function () {
+  showThumbnail(personInput, "thumbnail-person");
+});
+
+foodInput.addEventListener("change", function () {
+  showThumbnail(foodInput, "thumbnail-food");
+});
+
+animationInput.addEventListener("change", function () {
+  showThumbnail(animationInput, "thumbnail-animation");
+});
+
 function showThumbnail(input, id) {
-  var reader = new FileReader();
-  reader.onload = function (event) {
-    var image = document.getElementById(id);
-    image.src = event.target.result;
-    image.onload = function () {
-      var width = this.width;
-      var height = this.height;
-      if (width / height > 4 / 3) {
-        this.style.height = "100%";
-        this.style.width = "auto";
-      } else {
-        this.style.width = "100%";
-        this.style.height = "auto";
-      }
+  var file = input.files[0];
+  var imageType = /image.*/;
+
+  if (file && file.type.match(imageType)) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var image = document.getElementById(id);
+      image.src = event.target.result;
+      image.onload = function () {
+        var width = this.width;
+        var height = this.height;
+        if (width / height > 4 / 3) {
+          this.style.height = "100%";
+          this.style.width = "auto";
+        } else {
+          this.style.width = "100%";
+          this.style.height = "auto";
+        }
+      };
     };
-  };
-  if (input.files && input.files[0]) {
-    if (input.files[0].type.indexOf("image") === 0) {
-      reader.readAsDataURL(input.files[0]);
-    } else {
-      alert(
-        "選択したファイルは画像ではありません。画像ファイルを選択してください。"
-      );
-    }
+    reader.readAsDataURL(file);
   } else {
-    document.getElementById(id).src =
-      "https://i.pinimg.com/736x/37/cf/7c/37cf7c0e7257f6ef3509c9eeba3a924b.jpg";
+    alert(
+      "選択したファイルは画像ではありません。画像ファイルを選択してください。"
+    );
   }
 }
